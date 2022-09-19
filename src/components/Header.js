7f0e85e234b34df28a81eeee70c7c9db774b6cee
Mikael <mikaelangelopizzi@gmail.com>
2022-09-18 20:26:53 -0400

    [ADD] Add style to components with emotion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const HeaderContainer = styled.header`
+    background-color: #26c6da;
+    padding: 10px;
+    font-weight: bold;
+    color: #fff;
+`;
+
+const HeaderText = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
 
 const Header = ({ title }) => {
     return ( 
-        <header>
-            <h1>{title}</h1>
-        </header>
+        <HeaderContainer>
+            <HeaderText>{title}</HeaderText>
+        </HeaderContainer>
      );
 }
  
